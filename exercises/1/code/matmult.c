c02dd56ffd64664c800f9afcf420d5cc46d1872f
einar90 <einar.baumann@gmail.com>
2014-01-21 14:20:10 +0100

    Wrote skeleton

---
+++ exercises/1/code/matmult.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+double A, x, y;
+
+void MultiplyMatrices(double A, double x, double y)
+{
+  
+}
+
+int main(int argc, char const *argv[])
+{
+  MultiplyMatrices(A, x, y);
+  return 0;
+}

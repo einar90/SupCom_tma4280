5f65b90755de8203586089ab7af4ba044a8322ed
einar90 <einar.baumann@gmail.com>
2014-01-21 14:38:32 +0100

    Program complete

--- exercises/1/code/matmult.c
+++ exercises/1/code/matmult.c
@@ -1,11 +1,24 @@
 #include "stdio.h"
 #include "stdlib.h"
 
-double A, x, y;
+double y[3];
+double x[3] = {1.0, 1.0, 1.0};
+double A[][3] = { {0.3, 0.4, 0.3},
+                  {0.7, 0.1, 0.2},
+                  {0.5, 0.5, 0.0}};
 
-void MultiplyMatrices(double A, double x, double y)
+void MultiplyMatrices(double A[][3], double x[3], double y[3])
 {
-  
+  int i,j; // Loop counters
+  for (int i = 0; i < 3; ++i)
+  {
+    y[i] = 0.0; // Initializing to 0.0
+    for (int j = 0; j < 3; ++j)
+    {
+      y[i] += x[j] * A[i][j];
+    }
+  }
+  printf("y = [ %f, %f, %f ]\n", y[0], y[1], y[2]);
 }
 
 int main(int argc, char const *argv[])
